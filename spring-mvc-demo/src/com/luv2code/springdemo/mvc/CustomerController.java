0c8b44e306ce3a3be1ca1f4ab7e0dc7bca739a4d
Ali Dincer Dere <ali.dincer.dere@gmail.com>
2017-12-17 23:22:28 +0300

    trim spaces to null

--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/CustomerController.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/CustomerController.java
@@ -2,9 +2,12 @@ package com.luv2code.springdemo.mvc;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,6 +16,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class CustomerController {
 	
 
+	@InitBinder
+	public void initBinder(WebDataBinder webDataBinder) {
+		
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		
+		webDataBinder.registerCustomEditor(String.class,stringTrimmerEditor);
+	}
+	
+	
 	@RequestMapping("/showForm")
 	public String showForm(Model model) {
 		
